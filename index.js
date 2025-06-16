72f88ff8de9f4bb8933c2b2c0344555014f53fc7
dagensvimmerbymaster <dagensvimmerbymaster@gmail.com>
2025-06-16 11:39:56 +0200

    Update index.js
    Update android pushconfigs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// index.js – Uppgraderad för Parse Server v6.4.0
+// index.js – Uppgraderad för Parse Server v6.4.0 med dynamisk push config
 
 const express = require('express');
 const http = require('http');
@@ -11,31 +11,41 @@ if (!databaseUri) {
   console.log('⚠️ DATABASE_URI not specified, falling back to localhost.');
 }
 
-// Push-konfiguration
+// Push key path för ev. iOS
 const pushKeyPath = path.resolve(__dirname, './certificate/AuthKey-<keyID>.p8');
 console.log('Push key path:', pushKeyPath);
 
-const pushConfig = {
-  android: {
-    senderId: '',
-    apiKey: ''
+// Push-konfiguration för Android (enligt appId)
+const androidPushConfigs = {
+  'id-dagensvimmerby': {
+    senderId: '9966393092',
+    apiKey: 'AAAAAlILFwQ:APA91bFc35odIRUsaAFv58wDbO_3ram_yFk92npV9HfD3T-eT7rRXMsrq8601-Y6b4RPA44KcgQe8ANGoSucIImdIs0ZlLBYPyQzVBD3s5q8C9Wj5T-Fnk684Kl1I_iWxTJyrWoim8sr'
   },
-  ios: {
-    token: {
-      key: pushKeyPath,
-      keyId: '',
-      teamId: '' // The Team ID for your Apple developer account
-    },
-    topic: '', // Your app's bundle identifier
-    production: true
+  'id-dagenshultsfred': {
+    senderId: '725739666496',
+    apiKey: 'AAAAqPl0fEA:APA91bHp38ZbLlJSDBdv6t1_J_gXjpnMeaBOFPXu2sAG8qXoGIyXjf_NYafkhRiAvw7tqsIQmHm8BWxh7yM6X6O3ZjkoWWrfDp7TQ6X9_73edu9Ym-ENo1yPGmhbaSyeMJyMqQeimCGz'
+  },
+  'id-dagenskalmar': {
+    senderId: '400358993081',
+    apiKey: 'AAAAXTdBbLk:APA91bHU1sIB-7N_oC0fFhBd35eeGnzAiYIWD976qUJC3DMOy1iIgOHq-b-Wf49hSyFyr4wsspVrzQJv5lfibhsemza7uIBUti4k90W-UvWgOo5QUPi1u9q_fhV2rGL5Fb9ot6Vyfwnz'
+  },
+  'id-dagensvastervik': {
+    senderId: '259065804121',
+    apiKey: 'AAAAPFGGdVk:APA91bHAMUl0wuPGVHTk_Ynhsfb8hRYlclMgOnGWcXoiml5tfzKlJFS6_qhbfBOE8vuEft204ZWPj625I3wtW9LCxugaWs0cm710nUN88jJeXoph2cBJVPsiPcGmQ2NcEstHp7WRjnbW_d82395S-hUj0TKFnkOviQ'
   }
 };
+
+const appId = process.env.APP_ID || 'id-FAoIJ78ValGFwYdBWfxch7Fm';
+
+const pushConfig = androidPushConfigs[appId]
+  ? { android: androidPushConfigs[appId] }
+  : undefined;
 
 // Skapa Parse Server-instansen
 const parseServer = new ParseServer({
   databaseURI: databaseUri,
   cloud: process.env.CLOUD_CODE_MAIN || path.join(__dirname, '/cloud/main.js'),
-  appId: process.env.APP_ID || 'id-<ParseAppID>',
+  appId,
   masterKey: process.env.MASTER_KEY || 'key-<ParseMasterKey>',
   serverURL: process.env.SERVER_URL || 'http://<heroku-app>.herokuapp.com/parse',
   publicServerURL: process.env.PUBLIC_SERVER_URL || 'http://<heroku-app>.herokuapp.com/parse',
