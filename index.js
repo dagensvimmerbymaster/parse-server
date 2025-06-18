751c4812f9be6d3b517ac209fb1bae1fd34134bc
Elham <elham@MacBook-Pro-som-tillhor-Elham.local>
2025-06-18 17:17:42 +0200

    fix: added MaxConnections:3

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const pushAdapter = new PushAdapter({
       },
       topic: 'com.dagensvimmerbyab.DV',
       production: true,
-      maxConnections: 5,
+      maxConnections: 3,
       verbose: true
     }
   ]
