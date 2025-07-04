0aaee72653c62c7be0067896937e24e028e934a5
Elham <elham@MacBook-Pro-som-tillhor-Elham.local>
2025-07-04 15:19:27 +0200

    Lade till debug och error logging i index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const push = {
 // ----- Skapa ParseServer-instans -----
 const parseServer = new ParseServer({
   databaseURI: process.env.MONGODB_URI,
-  cloud: path.resolve('./cloud/main.js'),
+  cloud: process.env.CLOUD_CODE_MAIN || path.join(__dirname, 'cloud/main.js'),
   appId: process.env.APP_ID,
   masterKey: process.env.MASTER_KEY,
   serverURL: process.env.SERVER_URL,
